9ba86a7138257034c4165f1a7ff9bcb52e832539
Krzysztof Kula <krzychukula@gmail.com>
2014-07-25 13:32:42 +0200

    page 43

---
+++ RandomItems/BNRItem.h
@@ -0,0 +1,30 @@
+//
+//  BNRItem.h
+//  RandomItems
+//
+//  Created by Krzysztof Kula on 25.07.2014.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItem : NSObject
+{
+    NSString *_itemName;
+    NSString *_serialNumber;
+    int _valueInDollars;
+    NSDate *_dateCreated;
+}
+
+- (void)setItemName:(NSString *)str;
+- (NSString *)itemName;
+
+- (void)setSerialNumber:(NSString *)str;
+- (NSString *)serialNumber;
+
+- (void)setValueInDollars:(int)v;
+- (int)valueInDollars;
+
+- (NSDate *)dateCreated;
+
+@end
---
+++ RandomItems/BNRItem.m
@@ -0,0 +1,13 @@
+//
+//  BNRItem.m
+//  RandomItems
+//
+//  Created by Krzysztof Kula on 25.07.2014.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRItem.h"
+
+@implementation BNRItem
+
+@end

792a270ce435828deaaf4c74eab7752a04eb8aff
Krzysztof Kula <krzychukula@gmail.com>
2014-07-25 13:18:12 +0200

    page 37

---
+++ RandomItems/main.m
@@ -0,0 +1,37 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Krzysztof Kula on 25.07.2014.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        NSMutableArray *items = [[NSMutableArray alloc]init];
+        
+        [items addObject:@"One"];
+        [items addObject:@"Two"];
+        [items addObject:@"Three"];
+        
+        [items insertObject:@"Zero" atIndex:0];
+        
+//        for (int i = 0; i < [items count]; i++) {
+//            NSString *item = [items objectAtIndex:i];
+//            NSLog(@"%@", item);
+//        }
+        
+        for (NSString *item in items) {
+            NSLog(@"%@", item);
+        }
+        
+        items = nil;
+    }
+    return 0;
+}
+

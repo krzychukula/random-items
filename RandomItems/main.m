5d86b49603c097671249696db2216c69084fcee0
Krzysztof Kula <krzychukula@gmail.com>
2014-07-25 13:46:54 +0200

    page 46 - dot syntax

--- RandomItems/main.m
+++ RandomItems/main.m
@@ -32,11 +32,11 @@ int main(int argc, const char * argv[])
         }
         
         BNRItem *item = [[BNRItem alloc] init];
-        [item setItemName:@"Red Sofa"];
-        [item setSerialNumber:@"A1B2C"];
-        [item setValueInDollars:100];
-        NSLog(@"%@ %@ %@ %d", [item itemName], [item dateCreated],
-              [item serialNumber], [item valueInDollars]);
+        item.itemName = @"Red Sofa";
+        item.serialNumber = @"A1B2C";
+        item.valueInDollars = 5;
+        NSLog(@"%@ %@ %@ %d", item.itemName, item.dateCreated,
+              item.serialNumber, item.valueInDollars);
         
         items = nil;
     }

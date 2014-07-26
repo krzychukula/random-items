e9c96479ed59f1cd7722fffb5206c2c4c7d77d60
Krzysztof Kula <krzychukula@gmail.com>
2014-07-26 12:29:26 +0200

    page 58 class method

--- RandomItems/BNRItem.m
+++ RandomItems/BNRItem.m
@@ -10,6 +10,31 @@
 
 @implementation BNRItem
 
++ (instancetype)randomItem
+{
+    NSArray *randomAdjectiveList = @[@"Rusty", @"Fluffy", @"Shiny"];
+    NSArray *randomNounList = @[@"Bear", @"Spork", @"Mac"];
+    
+    NSInteger adjectiveIndex = arc4random() % [randomAdjectiveList count];
+    NSInteger nounIndex = arc4random() % [randomNounList count];
+    
+    NSString *randomName = [NSString stringWithFormat:@"%@ %@",
+                            [randomAdjectiveList objectAtIndex:adjectiveIndex],
+                            [randomNounList objectAtIndex:nounIndex]];
+    
+    int ranomValue = arc4random() % 100;
+    
+    NSString *randomSerialNumber = [NSString stringWithFormat:@"%c%c%c%c%c",
+                                    '0' + arc4random() % 10,
+                                    'A' + arc4random() % 26,
+                                    '0' + arc4random() % 10,
+                                    'A' + arc4random() % 26,
+                                    '0' + arc4random() % 10];
+    BNRItem *newItem = [[self alloc] initWithItemName:randomName valueInDollars:ranomValue serialNumber:randomSerialNumber];
+    
+    return newItem;
+}
+
 - (instancetype)initWithItemName:(id)name
                   valueInDollars:(int)value
                     serialNumber:(id)sNumber
